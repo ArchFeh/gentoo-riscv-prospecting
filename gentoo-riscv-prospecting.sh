945e0d6423d15f780d7e3acf0692c9ffcb9c31c7
Yongxiang Liang <tanekliang@gmail.com>
2022-05-30 11:49:09 +0800

    update 2022-05-30 11:49

--- gentoo-riscv-prospecting.sh
+++ gentoo-riscv-prospecting.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
-set -xe
+set -x
+set -e
 
 eix -# --in-overlay gentoo > pkgs.txt
 
-rm -rf 0_riscv.txt 1_arm64.txt 1_arm64_ver.txt 2_none.txt
-./keyworded pkgs.txt
+# test faild: dev-python/typish
+#rm -rf 0_riscv.txt 1_arm64.txt 1_arm64_ver.txt 2_none.txt
+#./keyworded pkgs.txt
 
 rm -f full.txt
-for pkg in $(cat 1_arm64.txt 2_none.txt | grep -Ev '(acct-user|acct-group)' | sort | uniq); do
+for pkg in $(cat pkgs.txt | grep -Ev '(acct-user|acct-group)' | sort | uniq); do
+    if [[ "$(equery -q m $pkg | grep riscv)" ]]; then
+        continue
+    fi
     dep_pkgs=$(zbt ls -p default/linux/riscv/20.0/rv64gc/lp64d/desktop/plasma/systemd $pkg)
     dep_pkgs_cnt=$(echo "$dep_pkgs" | wc -l)
     dep_pkgs_oneline=$(echo "$dep_pkgs" | tr '\n' ' ')
@@ -22,6 +27,6 @@ RE_MASK=
 cat full.txt | grep -Ev '(acct-user|acct-group|dev-java|dev-haskell|dev-ros|dev-ml|ros-meta|qtwebengine)' > todo.txt
 cat todo.txt | grep -Ev '(^dev-haskell|^dev-perl|^dev-php|^dev-ruby|^app-xemacs|^sci-biology|OpenStack)'  > todo2.txt
 
-git add 0_riscv.txt 1_arm64.txt 1_arm64_ver.txt 2_none.txt pkgs.txt full.txt todo.txt todo2.txt
+git add gentoo-riscv-prospecting.sh 0_riscv.txt 1_arm64.txt 1_arm64_ver.txt 2_none.txt pkgs.txt full.txt todo.txt todo2.txt
 git commit -m "update $(date '+%F %H:%M')"
 git push
